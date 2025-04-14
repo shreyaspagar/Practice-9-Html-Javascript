9bcad74b2f87879eb684c64aca4570dadcab2258
Shreyas Pagar <130757999+shreyaspagar@users.noreply.github.com>
2025-04-14 19:04:00 +0530

    Add files via upload

---
+++ app9.js
@@ -0,0 +1,13 @@
+let size = "M";
+if (size === "XL") {
+    console.log("price is RS. 250");
+}
+else if (size === "L") {
+    console.log("price is RS. 200");
+}
+else if (size === "M") {
+    console.log("price is RS. 10");
+}
+else {
+    console.log("price is RS. 50");
+}
